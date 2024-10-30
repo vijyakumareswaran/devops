021273c1c6fb056c89a032f8121d70cee74b0302
VijayakumarEswaran <vetri.vijay1991@gmail.com>
2024-10-31 03:52:32 +0530

    update

--- app/data.tf
+++ app/data.tf
@@ -1,7 +1,17 @@
+provider "aws" {
+  alias                       = "localstack"
+  region                      = "ap-southeast-1"
+  access_key                  = "test"
+  secret_access_key          = "test"
+  endpoint                   = "http://localhost:4566"
+  skip_credentials_validation = true
+  skip_get_ec2_platforms     = true
+}
+
 data "aws_caller_identity" "this" {
-  provider = aws.localstack  # Use a custom provider for LocalStack
+  provider = aws.localstack
 }
 
 data "aws_region" "this" {
-  provider = aws.localstack  # Use a custom provider for LocalStack
+  provider = aws.localstack
 }
