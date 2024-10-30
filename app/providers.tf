8e144d3da0609012c2366711d265a10307b95578
VijayakumarEswaran <vetri.vijay1991@gmail.com>
2024-10-31 01:33:53 +0530

    update

--- app/providers.tf
+++ app/providers.tf
@@ -13,6 +13,7 @@ provider "aws" {
   secret_key                  = "test"          # Use dummy values for LocalStack
   skip_region_validation      = true            # Skip region validation
   skip_credentials_validation  = true           # Skip credentials validation
+  skip_requesting_account_id   = true           # Skip requesting account ID
   endpoints {
     lambda = "http://localhost:4566"           # LocalStack endpoint for Lambda
     # Add other services here as needed
